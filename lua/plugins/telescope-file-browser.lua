730ff7511b2d65942b090fd3256f7b90beae8a60
Ivan Ignatov <ivanign04@gmail.com>
2025-08-12 20:24:33 +1200

    Set up mapping to open telescope-file-browser at current buffer path

--- lua/plugins/telescope-file-browser.lua
+++ lua/plugins/telescope-file-browser.lua
@@ -1,4 +1,11 @@
 return { -- File browser powered by telescope
     "nvim-telescope/telescope-file-browser.nvim",
     dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
+    config = function()
+        local picker = require("telescope").extensions.file_browser
+
+        vim.keymap.set("n", "<leader>fc", function()
+            picker.file_browser({ path = "%:p:h", cwd_to_path = true })
+        end, { noremap = true, desc = "Open at [C]urrent Buffer" })
+    end,
 }
